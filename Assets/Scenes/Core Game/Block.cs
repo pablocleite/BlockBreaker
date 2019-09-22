3b349e981ecb5e8d2017f75efb6130461b553795
Pablo Leite <pabloleite@Pablos-MacBook-Pro.local>
2019-09-22 18:24:04 -0300

    Adding logic to detect level ended, moving to the next level automatically.

--- Assets/Scenes/Core Game/Block.cs
+++ Assets/Scenes/Core Game/Block.cs
@@ -5,6 +5,13 @@ using UnityEngine;
 public class Block : MonoBehaviour {
 
     [SerializeField] AudioClip destructionSoundFX;
+    private Level level;
+
+    private void Start()
+    {
+        level = FindObjectOfType<Level>();
+        level.BlockAdded();
+    }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
@@ -14,6 +21,7 @@ public class Block : MonoBehaviour {
 
     private void OnDestroy()
     {
+        level.BlockDestroyed();
         AudioSource.PlayClipAtPoint(destructionSoundFX, Camera.main.transform.position);
     }
 }
---
+++ Assets/Scenes/Core Game/Level.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Level : MonoBehaviour
+{
+    private int breakableBlocks;
+
+    private SceneLoader sceneLoader;
+
+    private void Start()
+    {
+        sceneLoader = FindObjectOfType<SceneLoader>();
+    }
+
+
+    public void BlockAdded()
+    {
+        breakableBlocks++;
+    }
+
+    public void BlockDestroyed()
+    {
+        breakableBlocks--;
+        if(breakableBlocks == 0)
+        {
+            sceneLoader.loadNextScene();
+        }
+    }
+}
